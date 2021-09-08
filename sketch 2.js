42e31f17417059c4f39fabfba286bbbec420fe32
Atharva-jaiswal <89203446+Atharva-jaiswal@users.noreply.github.com>
2021-09-08 19:12:21 +0530

    Add files via upload

---
+++ sketch 2.js
@@ -0,0 +1,32 @@
+var ball
+
+function setup() {
+  createCanvas(400,400);
+  ball.createSprite(200,200,10,10)
+  }
+
+function draw() 
+{
+  background(30);
+  if (keyIsDown(RIGHT_ARROW))
+  {
+    background('red');
+  }
+  if (keyIsDown(LEFT_ARROW))
+  {
+    background('yellow');
+  }
+  if (keyIsDown(DOWN_ARROW))
+  {
+    background('green');
+  }
+  if (keyIsDown(UP_ARROW))
+  {
+    background('purple');
+  }
+  drawSprites()
+}
+
+
+
+
